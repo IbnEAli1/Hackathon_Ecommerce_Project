555dddbf5b588befa1a586ab014aed4f33900487
Syed Abbas Ai Zaidi <asabb625@gmail.com>
2024-12-16 21:09:53 +0500

    #

--- my-app-ecom/src/app/page.tsx
+++ my-app-ecom/src/app/page.tsx
@@ -1,9 +1,9 @@
-import BestSeller from "@/components/landingpage/BestSeller";
-import ClassicProduct from "@/components/landingpage/ClassicProduct";
-import EditorsPick from "@/components/landingpage/EditorsPick";
-import FeaturedPost from "@/components/landingpage/FeaturedPost";
-import Hero from "@/components/landingpage/Hero";
-import NeuralUniverse from "@/components/landingpage/NeuralUniverse";
+import Hero from "@/components/Hero";
+import EditorsPick from "@/components/EditorsPick";
+import BestSeller from "@/components/BestSeller";
+import ClassicProduct from "@/components/ClassicProduct";
+import NeuralUniverse from "@/components/NeuralUniverse";
+import FeaturedPost from "@/components/FeaturedPost";
 
 export default function Home() {
   return (
