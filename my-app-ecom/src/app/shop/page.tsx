98f526dd3280fac8ae5e12e19f83b833cf125c31
Syed Abbas Ai Zaidi <asabb625@gmail.com>
2024-12-17 20:04:58 +0500

    #

--- my-app-ecom/src/app/shop/page.tsx
+++ my-app-ecom/src/app/shop/page.tsx
@@ -1,5 +1,5 @@
-import Shop1 from "@/components/shop/Shop1";
-import ShopProducts from "@/components/shop/ShopProducts";
+import Shop1 from "@/components/shop2/Shop1";
+import ShopProducts from "@/components/shop2/ShopProducts";
 
 export default function Shop () {
     return (
