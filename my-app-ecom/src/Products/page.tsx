9f64657baff278b3aefb7f8fb0df083b62c71ed1
Syed Abbas Ai Zaidi <asabb625@gmail.com>
2026-02-12 13:39:51 +0500

    #

--- my-app-ecom/src/Products/page.tsx
+++ my-app-ecom/src/Products/page.tsx
@@ -69,7 +69,7 @@ const ProductCards: React.FC = () => {
     <div className="p-4">
       <h2 className="text-center text-slate-800 font-bold mt-4 mb-4">
         {" "}
-        Products from API's Data
+        Products from API&apos;s Data
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
